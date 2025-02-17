91a411ac7174f7ce8c41235191123199ce52c290
smile4kh <132769891+smile4kh@users.noreply.github.com>
2025-02-18 00:40:21 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,28 +2,14 @@ document.getElementById("analyzeBtn").addEventListener("click", function() {
     let fileInput = document.getElementById("ctgUpload");
     let analysisResult = document.getElementById("analysisResult");
 
-    if (fileInput.files.length === 0) {
-        analysisResult.textContent = "Please upload an image first.";
-        return;
-    }
-
     let file = fileInput.files[0];
     let formData = new FormData();
     formData.append("file", file);
 
-    fetch("/upload", {
-        method: "POST",
-        body: formData
-    })
+    fetch("/upload", { method: "POST", body: formData })
     .then(response => response.json())
     .then(data => {
-        if (data.error) {
-            analysisResult.textContent = "Error: " + data.error;
-        } else {
-            analysisResult.textContent = data.result;
-        }
-    })
-    .catch(error => {
-        analysisResult.textContent = "Error processing the image.";
+        analysisResult.textContent = data.result;
+        document.getElementById("ctgPlot").src = data.plot_url;
     });
 });
